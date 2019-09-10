f6a8d9aba5d57f1770be496f12efc7539a50d24a
Dan Krueger <krugster@yahoo.com>
2019-09-10 13:56:43 -0400

    Updating files

--- SitrusWebsite/SitrusScripts.js
+++ SitrusWebsite/SitrusScripts.js
@@ -33,10 +33,13 @@ function myCanvas() {
   img.src="./addOnFiles/goldBar.jpg";
 }
 
-function myClickFunction(username){
+function LoginButton(username, password){
     if(username == ""){
         alert("Please enter a valid username!");
-    }else{
+    }else if(password.length < 8){
+        alert("Password must be at least 8 characters long!");
+    }
+    else{
         alert("Welcome to Sitrus " + username + "!");
         myName = username;
         location.href='index.html';
@@ -44,12 +47,15 @@ function myClickFunction(username){
 }
 
 
-function myClickFunction2(signup, password, confirm){
+function SignUp(signup, password, confirm){
     if(signup == ""){
         alert("Looks like you did not complete your signup!")
     }
     else if(password != confirm){
         alert("Passwords do not match!")
+    }
+    else if(password.length < 8){
+        alert("Password must be at least 8 characters long!");
     }
     else{
         alert("Thank you for signing up " + signup + "!");
@@ -57,11 +63,17 @@ function myClickFunction2(signup, password, confirm){
     }
 }
 
-function myClickFunction3(passwordreset){
+function PasswordReset(passwordreset, confirm){
     if(passwordreset == ""){
         alert("Error you did not complete your passwordreset")
-    }else{
-        alert("Your Password has been successfully been resetted!");
+    }else if(passwordreset != confirm){
+        alert("Passwords do not match!");
+    }
+    else if(passwordreset.length < 8){
+        alert("Password must be at least 8 characters long!");
+    }
+    else{
+        alert("Your Password has been successfully been reset!");
         location.href='index.html';
     }
 }
