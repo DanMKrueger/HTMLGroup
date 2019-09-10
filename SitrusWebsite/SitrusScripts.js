202cc2d07dd116c419c277b1525011277c6b3435
Justin Rojas <jrdominican91@gmail.com>
2019-09-10 11:33:32 -0400

    updated files

--- SitrusWebsite/SitrusScripts.js
+++ SitrusWebsite/SitrusScripts.js
@@ -17,3 +17,12 @@ function myClickFunction(username){
         location.href='index.html';
     }
 }
+
+function myClickFunction2(signup){
+    if(signup == ""){
+        alert("Looks like you did not complete your signup!")
+    }else{
+        alert("Thank you for signing up " + signup + "!");
+        location.href='index.html';
+    }
+}
