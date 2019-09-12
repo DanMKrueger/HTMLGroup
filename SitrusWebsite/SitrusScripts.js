873ef8bb080ce062720012bdfb349cd58fbddcba
Dan Krueger <krugster@yahoo.com>
2019-09-12 14:32:00 -0400

    Updating files

--- SitrusWebsite/SitrusScripts.js
+++ SitrusWebsite/SitrusScripts.js
@@ -68,7 +68,20 @@ $(document).ready(function(){
 });
 //End Just Why N Section
 //--------------------------------------------------------------
+function EnterKeyPressed(){
+    var input = document.getElementById("Password");
 
+    // Execute a function when the user releases a key on the keyboard
+    input.addEventListener("keyup", function(event) {
+        // Number 13 is the "Enter" key on the keyboard
+        if (event.keyCode === 13) {
+            // Cancel the default action, if needed
+            event.preventDefault();
+            // Trigger the button element with a click
+            document.getElementById("Submit").click();
+        }
+    });
+}
 function LoginButton(username, password){
     if(username == ""){
         $('.loginPopup').empty();
