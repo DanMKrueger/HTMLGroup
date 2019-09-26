ebe0391836e893f29814008d10802fc15e2dddf2
GrimWasTaken <54084237+GrimWasTaken@users.noreply.github.com>
2019-09-26 14:52:51 -0400

    added enter event listener to JQuery section

--- SitrusWebsite/SitrusScripts.js
+++ SitrusWebsite/SitrusScripts.js
@@ -67,24 +67,17 @@ $(document).ready(function(){
                 });
 
                 //When a user tries to Sign Up, send the approrpiate HTTP Request for processing
-
+                $(document).keypress(function (e) {
+                    var key = e.which;
+                    if(key == 13)  
+                     {
+                        $("#Submit").click();
+                     }
+                   });   
 });
 //End Just Why N Section
 //--------------------------------------------------------------
-function EnterKeyPressed(){
-    var input = document.getElementById("Password");
 
-    // Execute a function when the user releases a key on the keyboard
-    input.addEventListener("keyup", function(event) {
-        // Number 13 is the "Enter" key on the keyboard
-        if (event.keyCode === 13) {
-            // Cancel the default action, if needed
-            event.preventDefault();
-            // Trigger the button element with a click
-            document.getElementById("Submit").click();
-        }
-    });
-}
 function LoginButton(username, password){
     if(username == ""){
         $('.loginPopup').empty();
